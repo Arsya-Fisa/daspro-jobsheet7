6c7d250fce76580aa374880fda80953c9509a95f
Arsya-Fisa <Arsya Fikri S>
2024-10-12 09:10:52 +0700

    percobaan 1

--- SiakadFor01.java
+++ SiakadFor01.java
@@ -4,7 +4,8 @@ public class SiakadFor01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        double nilai, tertinggi = 0, terendah = 100;
+        double nilai, tertinggi = 0, terendah = 100, lulus = 0, tidaklulus = 0;
+        
         
         for (int i = 1; i <= 10; i++){
             System.out.print("Masukan nilai mahasiswa ke-" + i + ": ");
@@ -15,9 +16,17 @@ public class SiakadFor01 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+            if (nilai > 60) {
+                lulus++;
+            }else{
+                tidaklulus++;
+            }
+
         }
         System.out.println("Nilai tertinggi: " + tertinggi);
         System.out.println("Nilai terendah: " + terendah);
+        System.out.println("lulus: " + lulus);
+        System.out.println("Tidak lulus: " + tidaklulus);
     }
 
     
